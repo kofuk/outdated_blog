8d849e0b5bdb99a1779daf88459fd93d4f910fab
Koki Fukuda <ko.fu.dev@gmail.com>
2020-01-17 10:33:47 +0900

    Fix error

--- src/entry_list_lifo.bash
+++ src/entry_list_lifo.bash
@@ -4,8 +4,11 @@ echo
 
 page="$(echo "$PATH_INFO" | sed -E 's@^/api/entry/list/lifo/([0-9]+)$@\1@')"
 
-find ./contents -mindepth 1 -maxdepth 1 -exec git log --format='%h'$'\t''{}'$'\t''{}'$'\t''%ad' \
-     --date='format:%Y%m%d%H%M'$'\t''%Y/%m/%d' \{\} \; | sort -rk2 | \
-    tail -n +$((page*10+1)) | head -n 10 | \
-    sed -E 's@([0-9a-f]+)\t([^\t]+)\t./contents/([^\t]+)\t([0-9]+)\t([0-9/]+)@echo "$(bash '"$src_dir"'/entry_title.bash "\2")\t/entry/\3/\t\5"@e' | \
-    column --separator=$'\t' --json --table-name=entry --table-columns=name,url,date
+(
+    cd "$src_dir/.."
+    find ./contents -mindepth 1 -maxdepth 1 -exec git log --format='%h'$'\t''{}'$'\t''{}'$'\t''%ad' \
+         --date='format:%Y%m%d%H%M'$'\t''%Y/%m/%d' \{\} \; | sort -rk2 | \
+        tail -n +$((page*10+1)) | head -n 10 | \
+        sed -E 's@([0-9a-f]+)\t([^\t]+)\t./contents/([^\t]+)\t([0-9]+)\t([0-9/]+)@echo "$(bash '"$src_dir"'/entry_title.bash "\2")\t/entry/\3/\t\5"@e' | \
+        column --separator=$'\t' --json --table-name=entry --table-columns=name,url,date
+)
